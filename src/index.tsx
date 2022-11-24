c26adf2207e4da94789d45312dc6b30b6b11130a
olegkrasilev <olegkrasilev@gmail.com>
2022-11-24 15:19:05 +0300

    fix(app):  minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.css'
-import App from './app'
+import './index.css';
+import App from './app';
 
-const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.querySelector('#root') as HTMLElement,
+);
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
