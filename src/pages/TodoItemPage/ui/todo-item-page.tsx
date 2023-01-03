e1079448aca079752b177ef46f4d7d0c14827406
Oleg Krasilev <65223358+olegkrasilev@users.noreply.github.com>
2023-01-03 11:35:34 +0300

    feat(app): :sparkles: add guard for router

--- src/pages/TodoItemPage/ui/todo-item-page.tsx
+++ src/pages/TodoItemPage/ui/todo-item-page.tsx
@@ -1,30 +1,55 @@
 /* eslint-disable unicorn/no-null */
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import css from './todo-item-page.module.scss';
 
 import { TodoCard } from 'entities/todo/ui/todo-card';
 import { useGetTodoByIDQuery } from 'shared/api';
+import { Spinner } from 'widgets/spinner';
+import { ErrorPage } from 'widgets/error-page';
 
 const TodoItemPage = () => {
   const { todoId } = useParams();
+  const navigate = useNavigate();
 
-  // @TODO Add guards
+  const { data, isError, isLoading, error } = useGetTodoByIDQuery(
+    Number(todoId),
+  );
 
-  const { data } = useGetTodoByIDQuery(Number(todoId));
+  if (error) {
+    const convertErrorType = error as { status: number };
+
+    if (convertErrorType.status === 404) {
+      navigate('/');
+
+      return null;
+    }
+  }
+
+  if (isError) {
+    return <ErrorPage />;
+  }
+
+  if (isLoading) {
+    return <Spinner isLoading={isLoading} />;
+  }
 
   if (!data) {
-    return null;
+    return <h1>This item doesnt exist</h1>;
   }
 
   const { completed, id, title } = data;
 
   return (
-    <TodoCard
-      id={id}
-      completed={completed}
-      title={title}
-    />
+    <>
+      <Spinner isLoading={isLoading} />
+      <TodoCard
+        id={id}
+        completed={completed}
+        title={title}
+      />
+      <button onClick={() => navigate('/')}>Back</button>
+    </>
   );
 };
 
